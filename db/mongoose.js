8e72a34d371e34f69e10ae2445a344943f4e2563
Suneil Basu <suneilbasu@users.noreply.github.com>
2022-03-29 08:08:35 +0100

    Update mongoose.js
    can't have all the class working on the same database

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,7 @@ main().catch((err) => console.log(err))
 
 async function main() {
   await mongoose.connect(
-    // "mongodb+srv://suneilbasu:backWell@cluster0.bpc1d.mongodb.net/tzar?retryWrites=true&w=majority"
+    // "mongodb+srv://suneilbasu:somethingElse@cluster0.bpc1d.mongodb.net/tzar?retryWrites=true&w=majority"
     "mongodb+srv://superuser:p9ZV8mrRqzNG7Q3@cluster0.9fxz9.mongodb.net/tzar?retryWrites=true&w=majority"
     // "mongodb://locahost:27017/retry"
 
